8ee98387cc0b88e75ab451b9f6f20ecb7f83115a
Hironsan <light.tree.1.13@gmail.com>
2016-07-19 16:54:11 +0900

    implement pgm_reader

--- capture.py
+++ capture.py
@@ -1,9 +1,10 @@
 # -*- coding:utf-8 -*-
 #webカメラの映像から顔を探し白の枠線をつけて保存するプログラム
 
-import cv2
 import threading
 from datetime import datetime
+import cv2
+
 
 class FaceThread(threading.Thread):
     def __init__(self, frame):
@@ -12,47 +13,47 @@ class FaceThread(threading.Thread):
         self._frame = frame
 
     def run(self):
-        #グレースケール変換
-        self._frame_gray = cv2.cvtColor(self._frame, cv2.cv.CV_BGR2GRAY)
+        # グレースケール変換
+        self._frame_gray = cv2.cvtColor(self._frame, cv2.COLOR_BGR2GRAY)
 
-        #カスケード分類器の特徴量を取得する
+        # カスケード分類器の特徴量を取得する
         self._cascade = cv2.CascadeClassifier(self._cascade_path)
 
-        #物体認識（顔認識）の実行
+        # 物体認識（顔認識）の実行
         self._facerect = self._cascade.detectMultiScale(self._frame_gray, scaleFactor=1.2, minNeighbors=3, minSize=(10, 10))
 
         if len(self._facerect) > 0:
-            print '顔が検出されました。'
-            self._color = (255, 255, 255) #白
+            print('face detected')
+            self._color = (255, 255, 255) # 白
             for self._rect in self._facerect:
-                #検出した顔を囲む矩形の作成
+                # 検出した顔を囲む矩形の作成
                 cv2.rectangle(self._frame, tuple(self._rect[0:2]),tuple(self._rect[0:2] + self._rect[2:4]), self._color, thickness=2)
 
-            #現在の時間を取得
+            # 現在の時間を取得
             self._now = datetime.now().strftime('%Y%m%d%H%M%S')
-            #認識結果の保存
+            # 認識結果の保存
             self._image_path = self._now + '.jpg'
             cv2.imwrite(self._image_path, self._frame)
 
 # カメラをキャプチャ開始
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
 
-    #frameを表示
+    # frameを表示
     cv2.imshow('camera capture', frame)
 
     if(threading.activeCount() == 1):
         th = FaceThread(frame)
         th.start()
 
-    #10msecキー入力待ち
+    # 10msecキー入力待ち
     k = cv2.waitKey(10)
-    #Escキーを押されたら終了
+    # Escキーを押されたら終了
     if k == 27:
         break
 
-#キャプチャを終了
+# キャプチャを終了
 cap.release()
 cv2.destroyAllWindows()
--- pgm_reader.py
+++ pgm_reader.py
@@ -1,21 +1,31 @@
 import os
-# import cv2                                                                                                                                                                             
+import cv2
 
 
 def read_image(file_path):
-    print(file_path)
-    # cv2.read(file_path, 1)  # grayscale                                                                                                                                                
-    pass
+    image = cv2.imread(file_path, 0)  # grayscale
+    print(image)
+    print(image.shape)
+    return image
 
-
+images = []
+labels = []
 def traverse_dir(path):
     for file_or_dir in os.listdir(path):
         abs_path = os.path.abspath(os.path.join(path, file_or_dir))
-        if os.path.isdir(abs_path):  # dir                                                                                                                                               
+        if os.path.isdir(abs_path):  # dir
             traverse_dir(abs_path)
-        else:                        # file                                                                                                                                              
-            if abs_path.endswith('pgm'):
-                read_image(abs_path)
+        else:                        # file
+            if file_or_dir.endswith('.pgm'):
+                image = read_image(abs_path)
+                images.append(image)
+                labels.append(path)
+    return images, labels
 
 if __name__ == '__main__':
-    traverse_dir("./data")
+    images, labels = traverse_dir("./data")
+    print(len(images))
+    print(len(labels))
+    print(labels)
+    # print(images.shape)
+    # print(labels.shape)
