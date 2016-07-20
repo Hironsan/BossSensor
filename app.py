d8d1ad395e3dfd54aca98edc5caf0c81b8ac233d
Hironsan <light.tree.1.13@gmail.com>
2016-07-20 17:13:53 +0900

    implement CNN

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from pgm_reader import read_data_sets
 import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
 
-
+#mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+#x = tf.placeholder(tf.float32, shape=[None, 784])
+#y_ = tf.placeholder(tf.float32, shape=[None, 10])
 mnist = read_data_sets()
-sess = tf.InteractiveSession()
 x = tf.placeholder(tf.float32, shape=[None, 644])
 y_ = tf.placeholder(tf.float32, shape=[None, 20])
+sess = tf.InteractiveSession()
+"""
+mnist = read_data_sets()
+sess = tf.InteractiveSession()
+
+x = tf.placeholder(tf.float32, shape=[None, 644])
+y_ = tf.placeholder(tf.float32, shape=[None, 20])
+
 W = tf.Variable(tf.zeros([644, 20]))
 b = tf.Variable(tf.zeros([20]))
 sess.run(tf.initialize_all_variables())
@@ -14,7 +24,7 @@ cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=
 
 # Train the model
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-for i in range(1000):
+for i in range(10000):
     batch = mnist.train.next_batch(10)
     train_step.run(feed_dict={x: batch[0], y_: batch[1]})
 
@@ -22,3 +32,56 @@ for i in range(1000):
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+"""
+
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+
+W_conv1 = weight_variable([5, 5, 1, 32])
+b_conv1 = bias_variable([32])
+x_image = tf.reshape(x, [-1, 23, 28, 1])
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
+W_conv2 = weight_variable([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+W_fc1 = weight_variable([7 * 7 * 64, 1024])
+b_fc1 = bias_variable([1024])
+
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+W_fc2 = weight_variable([1024, 20])
+b_fc2 = bias_variable([20])
+
+y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+sess.run(tf.initialize_all_variables())
+for i in range(20000):
+    batch = mnist.train.next_batch(50)
+    if i % 100 == 0:
+        train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_: batch[1], keep_prob: 1.0})
+        print("step %d, training accuracy %g"%(i, train_accuracy))
+    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+print("test accuracy %g"%accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
