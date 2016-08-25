65c064cdc519d2eb6b4b370b5049f0df90934bc8
Hironsan <light.tree.1.13@gmail.com>
2016-08-25 17:35:21 +0900

    update app.py
    split code into three part: inference, loss and trainer

--- app.py
+++ app.py
@@ -1,90 +1,85 @@
-from pgm_reader import read_data_sets
 import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
-
-#mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-#x = tf.placeholder(tf.float32, shape=[None, 784])
-#y_ = tf.placeholder(tf.float32, shape=[None, 10])
-mnist = read_data_sets()
-#x = tf.placeholder(tf.float32, shape=[None, 644])
-#x = tf.placeholder(tf.float32, shape=[None, 4096])
-x = tf.placeholder(tf.float32, shape=[None, 1024])
-y_ = tf.placeholder(tf.float32, shape=[None, 20])
-sess = tf.InteractiveSession()
-"""
-#mnist = read_data_sets()
-#sess = tf.InteractiveSession()
-
-#x = tf.placeholder(tf.float32, shape=[None, 644])
-#y_ = tf.placeholder(tf.float32, shape=[None, 20])
-
-W = tf.Variable(tf.zeros([4096, 20]))
-b = tf.Variable(tf.zeros([20]))
-sess.run(tf.initialize_all_variables())
-y = tf.nn.softmax(tf.matmul(x, W) + b)
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
-
-# Train the model
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-for i in range(10000):
-    batch = mnist.train.next_batch(10)
-    train_step.run(feed_dict={x: batch[0], y_: batch[1]})
-
-# Evaluate the model
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
-"""
+import input_data
 
 
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial)
 
+
 def bias_variable(shape):
     initial = tf.constant(0.1, shape=shape)
     return tf.Variable(initial)
 
+
 def conv2d(x, W):
     return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
 
 def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 
-W_conv1 = weight_variable([5, 5, 1, 32])
-b_conv1 = bias_variable([32])
-#x_image = tf.reshape(x, [-1, 64, 64, 1])
-x_image = tf.reshape(x, [-1, 32, 32, 1])
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
-W_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
-W_fc1 = weight_variable([8*8*64, 1024])
-b_fc1 = bias_variable([1024])
+def inference(input_placeholder, keep_prob):
+    W_conv1 = weight_variable([5, 5, 1, 32])
+    b_conv1 = bias_variable([32])
 
-h_pool2_flat = tf.reshape(h_pool2, [-1, 8*8*64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+    x_image = tf.reshape(input_placeholder, [-1, 32, 32, 1])
+
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+    h_pool1 = max_pool_2x2(h_conv1)
+
+    W_conv2 = weight_variable([5, 5, 32, 64])
+    b_conv2 = bias_variable([64])
+
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    h_pool2 = max_pool_2x2(h_conv2)
+
+    W_fc1 = weight_variable([8 * 8 * 64, 1024])
+    b_fc1 = bias_variable([1024])
+
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 8 * 8 * 64])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+    W_fc2 = weight_variable([1024, 40])
+    b_fc2 = bias_variable([40])
+
+    y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+
+    return y_conv
+
+
+def loss(output, supervisor_labels_placeholder):
+    cross_entropy = tf.reduce_mean(-tf.reduce_sum(supervisor_labels_placeholder * tf.log(output), reduction_indices=[1]))
+    return cross_entropy
+
+
+def training(loss):
+    train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
+    return train_step
+
+
+dataset = input_data.read_data_sets('data/olivettifaces.mat')
+x = tf.placeholder(tf.float32, shape=[None, 1024])
+y_ = tf.placeholder(tf.float32, shape=[None, 40])
 keep_prob = tf.placeholder(tf.float32)
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-W_fc2 = weight_variable([1024, 20])
-b_fc2 = bias_variable([20])
+with tf.Session() as sess:
+    output = inference(x, keep_prob)
+    loss = loss(output, y_)
+    training_op = training(loss)
 
-y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+    init = tf.initialize_all_variables()
+    sess.run(init)
 
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-sess.run(tf.initialize_all_variables())
-for i in range(1000):
-    batch = mnist.train.next_batch(50)
-    if i % 100 == 0:
-        train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
-        print("step %d, training accuracy %g"%(i, train_accuracy))
-    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+    for step in range(1000):
+        batch = dataset.train.next_batch(40)
+        sess.run(training_op, feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+        if step % 100 == 0:
+            print(sess.run(loss, feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0}))
 
-print("test accuracy %g" % accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+    correct_prediction = tf.equal(tf.argmax(output, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    print('test accuracy %g' % accuracy.eval(feed_dict={x: dataset.test.images, y_: dataset.test.labels, keep_prob: 1.0}))
--- input_data.py
+++ input_data.py
@@ -1,62 +1,28 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-import gzip
-
-import numpy
-from six.moves import xrange  # pylint: disable=redefined-builtin
-
-from tensorflow.contrib.learn.python.learn.datasets import base
+import random
+import cv2
+import numpy as np
 from tensorflow.python.framework import dtypes
-from tensorflow.python.platform import gfile
-
-SOURCE_URL = 'http://yann.lecun.com/exdb/mnist/'
-
-
-def _read32(bytestream):
-    dt = numpy.dtype(numpy.uint32).newbyteorder('>')
-    return numpy.frombuffer(bytestream.read(4), dtype=dt)[0]
-
-
-def extract_images(filename):
-    """Extract the images into a 4D uint8 numpy array [index, y, x, depth]."""
-    print('Extracting', filename)
-    with gfile.Open(filename, 'rb') as f, gzip.GzipFile(fileobj=f) as bytestream:
-        magic = _read32(bytestream)
-        if magic != 2051:
-            raise ValueError('Invalid magic number %d in MNIST image file: %s' %(magic, filename))
-        num_images = _read32(bytestream)
-        rows = _read32(bytestream)
-        cols = _read32(bytestream)
-        buf = bytestream.read(rows * cols * num_images)
-        data = numpy.frombuffer(buf, dtype=numpy.uint8)
-        data = data.reshape(num_images, rows, cols, 1)
-        return data
+from tensorflow.contrib.learn.python.learn.datasets import base
+import scipy.io
 
 
 def dense_to_one_hot(labels_dense, num_classes):
     """Convert class labels from scalars to one-hot vectors."""
-    num_labels = labels_dense.shape[0]
-    index_offset = numpy.arange(num_labels) * num_classes
-    labels_one_hot = numpy.zeros((num_labels, num_classes))
-    labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
+    num_labels = len(labels_dense)
+    labels_one_hot = np.zeros((num_labels, num_classes))
+    label_list = list(set(labels_dense))
+    for i, label in enumerate(labels_dense):
+        labels_one_hot[i][label_list.index(label)] = 1
     return labels_one_hot
 
 
-def extract_labels(filename, one_hot=False, num_classes=10):
-    """Extract the labels into a 1D uint8 numpy array [index]."""
-    print('Extracting', filename)
-    with gfile.Open(filename, 'rb') as f, gzip.GzipFile(fileobj=f) as bytestream:
-        magic = _read32(bytestream)
-        if magic != 2049:
-            raise ValueError('Invalid magic number %d in MNIST label file: %s' %(magic, filename))
-        num_items = _read32(bytestream)
-        buf = bytestream.read(num_items)
-        labels = numpy.frombuffer(buf, dtype=numpy.uint8)
-        if one_hot:
-            return dense_to_one_hot(labels, num_classes)
-        return labels
+def extract_data(path):
+    mat = scipy.io.loadmat(path)
+    images = mat['faces'].T
+    images = np.array([np.reshape(cv2.resize(np.reshape(image, (64, 64)), (32, 32)), -1) for image in images])
+    labels = [i//10 for i in range(images.shape[0])]
+    labels_one_hot = dense_to_one_hot(labels, len(set(labels)))
+    return images, labels_one_hot
 
 
 class DataSet(object):
@@ -64,34 +30,19 @@ class DataSet(object):
     def __init__(self,
                  images,
                  labels,
-                 fake_data=False,
-                 one_hot=False,
                  dtype=dtypes.float32,
                  reshape=True):
-        """Construct a DataSet.
-        one_hot arg is used only if fake_data is true.  `dtype` can be either
-        `uint8` to leave the input as `[0, 255]`, or `float32` to rescale into
-        `[0, 1]`.
-        """
+
         dtype = dtypes.as_dtype(dtype).base_dtype
         if dtype not in (dtypes.uint8, dtypes.float32):
             raise TypeError('Invalid image dtype %r, expected uint8 or float32' %dtype)
-        if fake_data:
-            self._num_examples = 10000
-            self.one_hot = one_hot
-        else:
-            assert images.shape[0] == labels.shape[0], ('images.shape: %s labels.shape: %s' % (images.shape, labels.shape))
-            self._num_examples = images.shape[0]
 
-            # Convert shape from [num examples, rows, columns, depth]
-            # to [num examples, rows*columns] (assuming depth == 1)
-            if reshape:
-                assert images.shape[3] == 1
-                images = images.reshape(images.shape[0], images.shape[1] * images.shape[2])
-            if dtype == dtypes.float32:
-                # Convert from [0, 255] -> [0.0, 1.0].
-                images = images.astype(numpy.float32)
-                images = numpy.multiply(images, 1.0 / 255.0)
+        self._num_examples = images.shape[0]
+
+        if dtype == dtypes.float32:
+            # Convert from [0, 255] -> [0.0, 1.0].
+            images = images.astype(np.float32)
+            images = np.multiply(images, 1.0 / 255.0)
         self._images = images
         self._labels = labels
         self._epochs_completed = 0
@@ -115,21 +66,14 @@ class DataSet(object):
 
     def next_batch(self, batch_size, fake_data=False):
         """Return the next `batch_size` examples from this data set."""
-        if fake_data:
-            fake_image = [1] * 784
-            if self.one_hot:
-                fake_label = [1] + [0] * 9
-            else:
-                fake_label = 0
-            return [fake_image for _ in xrange(batch_size)], [fake_label for _ in xrange(batch_size)]
         start = self._index_in_epoch
         self._index_in_epoch += batch_size
         if self._index_in_epoch > self._num_examples:
             # Finished epoch
             self._epochs_completed += 1
             # Shuffle the data
-            perm = numpy.arange(self._num_examples)
-            numpy.random.shuffle(perm)
+            perm = np.arange(self._num_examples)
+            np.random.shuffle(perm)
             self._images = self._images[perm]
             self._labels = self._labels[perm]
             # Start next epoch
@@ -140,57 +84,27 @@ class DataSet(object):
         return self._images[start:end], self._labels[start:end]
 
 
-def read_data_sets(train_dir,
-                   fake_data=False,
-                   one_hot=False,
-                   dtype=dtypes.float32,
-                   reshape=True):
-    if fake_data:
+def read_data_sets(path, dtype=dtypes.float32, reshape=False):
+    images, labels = extract_data(path)
+    for i in range(images.shape[0]):
+        j = random.randint(i, images.shape[0]-1)
+        images[i], images[j] = images[j], images[i]
+        labels[i], labels[j] = labels[j], labels[i]
 
-        def fake():
-            return DataSet([], [], fake_data=True, one_hot=one_hot, dtype=dtype)
+    num_images = images.shape[0]
 
-        train = fake()
-        validation = fake()
-        test = fake()
-        return base.Datasets(train=train, validation=validation, test=test)
+    TRAIN_SIZE = int(num_images * 0.8)
+    VALIDATION_SIZE = int(num_images * 0.1)
 
-    TRAIN_IMAGES = 'train-images-idx3-ubyte.gz'
-    TRAIN_LABELS = 'train-labels-idx1-ubyte.gz'
-    TEST_IMAGES = 't10k-images-idx3-ubyte.gz'
-    TEST_LABELS = 't10k-labels-idx1-ubyte.gz'
-    VALIDATION_SIZE = 5000
-
-    local_file = base.maybe_download(TRAIN_IMAGES, train_dir,
-                                   SOURCE_URL + TRAIN_IMAGES)
-    train_images = extract_images(local_file)
-
-    local_file = base.maybe_download(TRAIN_LABELS, train_dir,
-                                   SOURCE_URL + TRAIN_LABELS)
-    train_labels = extract_labels(local_file, one_hot=one_hot)
-
-    local_file = base.maybe_download(TEST_IMAGES, train_dir,
-                                   SOURCE_URL + TEST_IMAGES)
-    test_images = extract_images(local_file)
-
-    local_file = base.maybe_download(TEST_LABELS, train_dir,
-                                   SOURCE_URL + TEST_LABELS)
-    test_labels = extract_labels(local_file, one_hot=one_hot)
-
-    validation_images = train_images[:VALIDATION_SIZE]
-    validation_labels = train_labels[:VALIDATION_SIZE]
-    train_images = train_images[VALIDATION_SIZE:]
-    train_labels = train_labels[VALIDATION_SIZE:]
+    train_images = images[:TRAIN_SIZE]
+    train_labels = labels[:TRAIN_SIZE]
+    validation_images = images[TRAIN_SIZE:TRAIN_SIZE+VALIDATION_SIZE]
+    validation_labels = labels[TRAIN_SIZE:TRAIN_SIZE+VALIDATION_SIZE]
+    test_images = images[TRAIN_SIZE+VALIDATION_SIZE:]
+    test_labels = labels[TRAIN_SIZE+VALIDATION_SIZE:]
 
     train = DataSet(train_images, train_labels, dtype=dtype, reshape=reshape)
-    validation = DataSet(validation_images,
-                         validation_labels,
-                         dtype=dtype,
-                         reshape=reshape)
+    validation = DataSet(validation_images, validation_labels, dtype=dtype, reshape=reshape)
     test = DataSet(test_images, test_labels, dtype=dtype, reshape=reshape)
 
     return base.Datasets(train=train, validation=validation, test=test)
-
-
-def load_mnist():
-    return read_data_sets('MNIST_data')
