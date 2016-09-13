cc626971235f3a59451f1ab7a000c3bbb23c8b1f
h-nakayama <light.tree.1.13@gmail.com>
2016-09-14 06:20:57 +0900

    implement keras version model by class

--- trainer.py
+++ trainer.py
@@ -10,18 +10,21 @@ from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.optimizers import SGD
 from keras.utils import np_utils
+from keras.models import load_model
 from boss_input import extract_data
 import tensorflow as tf
-batch_size = 32
-nb_classes = 2
-nb_epoch = 1000
-data_augmentation = True
+
+
+#batch_size = 32
+#nb_classes = 2
+#nb_epoch = 1000
+#data_augmentation = True
 
 # input image dimensions
 img_rows, img_cols = 32, 32
 # the CIFAR10 images are RGB
 img_channels = 3
-
+"""
 with tf.Session() as sess:
     images, labels = extract_data('./data/', sess)
 labels = np.reshape(labels, [-1])
@@ -107,23 +110,121 @@ else:
                         samples_per_epoch=X_train.shape[0],
                         nb_epoch=nb_epoch,
                         validation_data=(X_test, Y_test))
+"""
+
+
+class Dataset(object):
+
+    def __init__(self):
+        self.X_train = None
+        self.X_test = None
+        self.Y_train = None
+        self.Y_test = None
+
+    def read(self, img_rows, img_cols, img_channels, nb_classes):
+        with tf.Session() as sess:
+            images, labels = extract_data('./data/', sess)
+        labels = np.reshape(labels, [-1])
+        # numpy.reshape
+        X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.2, random_state=random.randint(0, 100))
+        X_train = X_train.reshape(X_train.shape[0], 3, img_rows, img_cols)
+        X_test = X_test.reshape(X_test.shape[0], 3, img_rows, img_cols)
+
+        # the data, shuffled and split between train and test sets
+        #(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+        print('X_train shape:', X_train.shape)
+        print(X_train.shape[0], 'train samples')
+        print(X_test.shape[0], 'test samples')
+
+        # convert class vectors to binary class matrices
+        Y_train = np_utils.to_categorical(y_train, nb_classes)
+        Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+        X_train = X_train.astype('float32')
+        X_test = X_test.astype('float32')
+        X_train /= 255
+        X_test /= 255
+
+        self.X_train = X_train
+        self.X_test = X_test
+        self.Y_train = Y_train
+        self.Y_test = Y_test
+
 
 class Model(object):
 
     def __init__(self):
-        pass
+        self.model = None
 
-    def build_model(self):
-        pass
+    def build_model(self, nb_classes):
+        self.model = Sequential()
 
-    def train(self):
-        pass
+        self.model.add(Convolution2D(32, 3, 3, border_mode='same', input_shape=X_train.shape[1:]))
+        self.model.add(Activation('relu'))
+        self.model.add(Convolution2D(32, 3, 3))
+        self.model.add(Activation('relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.25))
 
-    def save(self):
-        pass
+        self.model.add(Convolution2D(64, 3, 3, border_mode='same'))
+        self.model.add(Activation('relu'))
+        self.model.add(Convolution2D(64, 3, 3))
+        self.model.add(Activation('relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.25))
 
-    def load(self):
-        pass
+        self.model.add(Flatten())
+        self.model.add(Dense(512))
+        self.model.add(Activation('relu'))
+        self.model.add(Dropout(0.5))
+        self.model.add(Dense(nb_classes))
+        self.model.add(Activation('softmax'))
+
+    def train(self, batch_size, nb_epoch, data_augmentation=True):
+        # let's train the model using SGD + momentum (how original).
+        sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+        self.model.compile(loss='categorical_crossentropy',
+                           optimizer=sgd,
+                           metrics=['accuracy'])
+        if not data_augmentation:
+            print('Not using data augmentation.')
+            self.model.fit(X_train, Y_train,
+                           batch_size=batch_size,
+                           nb_epoch=nb_epoch,
+                           validation_data=(X_test, Y_test),
+                           shuffle=True)
+        else:
+            print('Using real-time data augmentation.')
+
+            # this will do preprocessing and realtime data augmentation
+            datagen = ImageDataGenerator(
+                featurewise_center=False,             # set input mean to 0 over the dataset
+                samplewise_center=False,              # set each sample mean to 0
+                featurewise_std_normalization=False,  # divide inputs by std of the dataset
+                samplewise_std_normalization=False,   # divide each input by its std
+                zca_whitening=False,                  # apply ZCA whitening
+                rotation_range=0,                     # randomly rotate images in the range (degrees, 0 to 180)
+                width_shift_range=0.1,                # randomly shift images horizontally (fraction of total width)
+                height_shift_range=0.1,               # randomly shift images vertically (fraction of total height)
+                horizontal_flip=True,                 # randomly flip images
+                vertical_flip=False)                  # randomly flip images
+
+            # compute quantities required for featurewise normalization
+            # (std, mean, and principal components if ZCA whitening is applied)
+            datagen.fit(X_train)
+
+            # fit the model on the batches generated by datagen.flow()
+            self.model.fit_generator(datagen.flow(X_train, Y_train,
+                                                  batch_size=batch_size),
+                                     samples_per_epoch=X_train.shape[0],
+                                     nb_epoch=nb_epoch,
+                                     validation_data=(X_test, Y_test))
+
+    def save(self, file_path):
+        self.model.save(file_path)
+
+    def load(self, file_path):
+        self.model = load_model(file_path)
 
     def predict(self):
         pass
